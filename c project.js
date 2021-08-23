b6368af31bcc4f1735389d18313b918ced8ab3b7
KripaDhiraj <86722435+KripaDhiraj@users.noreply.github.com>
2021-08-23 11:13:09 +0530

    Add files via upload

---
+++ c project.js
@@ -0,0 +1,47 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Constraint = Matter.Constraint;
+
+var roof;
+//Create multiple bobs, mulitple ropes varibale here
+createCanvas (760,560,20,20)
+var ropes
+
+function setup() {
+	createCanvas(800, 600);
+	rectMode(CENTER);
+
+	engine = Engine.create();
+	world = engine.world;
+
+	var roof_options={
+		isStatic:true			
+	}
+
+	roof = Bodies.rectangle(400,100,230,20,roof_options);
+    World.add(world,roof);
+
+	Engine.run(engine);
+	
+  
+}
+
+function draw() {
+  rectMode(CENTER);
+  background("#99004d");
+
+  rect(roof.position.x,roof.position.y,230,20);
+
+  //call display() to show ropes here
+createImage('display');
+  
+  //create ellipse shape for multiple bobs here
+createCanvas(' bobs')
+ 
+}
+
+//Write keyPressed function and apply force on pressing up_arrow key on the first bob.
+if (keydown('UP_ARROW'));
+createCanvas('the first bob');
